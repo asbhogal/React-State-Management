d5ab0420ff74711e4dd8144a184b38ba104928f2
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-06-17 13:32:17 +0100

    added header, additions to styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,28 @@
 import { Link } from "react-router-dom";
+import Header from "../components/Header";
 
 const Home = () => {
   return (
-    <nav className="navbar">
-      <ul>
-        <li>
-          <Link to="/UseStateHook">useState</Link>
-        </li>
-        <li>useReducer</li>
-        <li>useMemo & useCallback</li>
-        <li>useEffect</li>
-        <li>useRef</li>
-        <li>Context & Custom Hooks</li>
-        <li>React Query & React Location</li>
-        <li>Zustand</li>
-        <li>Valtio</li>
-        <li>Jotai</li>
-        <li>Redux</li>
-      </ul>
-    </nav>
+    <>
+      <Header />
+      <main className="container">
+        <ul>
+          <li>
+            <Link to="/UseStateHook">useState</Link>
+          </li>
+          <li>useReducer</li>
+          <li>useMemo & useCallback</li>
+          <li>useEffect</li>
+          <li>useRef</li>
+          <li>Context & Custom Hooks</li>
+          <li>React Query & React Location</li>
+          <li>Zustand</li>
+          <li>Valtio</li>
+          <li>Jotai</li>
+          <li>Redux</li>
+        </ul>
+      </main>
+    </>
   );
 };
 
--- src/pages/useStateHook.jsx
+++ src/pages/useStateHook.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Header from "../components/Header";
 
 export default function UseStateHook() {
   const [count, setCount] = useState(10);
@@ -8,43 +9,46 @@ export default function UseStateHook() {
   }
 
   return (
-    <div>
-      <p>
-        const [count, setCount] = useState(10);
-        <br />
-        <br />
-        The output of useState() is an array. The first item is the current
-        value of the piece of state (here, &quot;count&quot;) and the second
-        item is the setter function:<br></br>
-        <br></br>const [count, setCount] = useState(); <br></br>
-        <br></br>useState(10) creates an array with the value 10. Within this
-        array, the first item is the current value of that piece of state, which
-        is assigned to the variable &quot;count&quot; Here in this declaration,
-        the second item in this array is the &quot;setter&quot; function called
-        &quot;setCount&quot; which is called at a later point in order to set
-        the piece of state (e.g. to a different value)
-        <br />
-        <br />
-        <button onClick={incrementValue}>Count = {count}</button>
-        <br />
-        <br />
-        The button is assigned the current value of count.
-        <br />
-        <br />
-        To increment this when the button is clicked, append the onClick event
-        attribute and pass it a function, for e.g.:
-        <br />
-        <br />
-        {`onClick={incrementValue}`}
-        <br />
-        <br />
-        Then before the return():
-        <br />
-        <br />
-        {`function incrementValue() {
+    <>
+      <Header />
+      <div>
+        <p>
+          const [count, setCount] = useState(10);
+          <br />
+          <br />
+          The output of useState() is an array. The first item is the current
+          value of the piece of state (here, &quot;count&quot;) and the second
+          item is the setter function:<br></br>
+          <br></br>const [count, setCount] = useState(); <br></br>
+          <br></br>useState(10) creates an array with the value 10. Within this
+          array, the first item is the current value of that piece of state,
+          which is assigned to the variable &quot;count&quot; Here in this
+          declaration, the second item in this array is the &quot;setter&quot;
+          function called &quot;setCount&quot; which is called at a later point
+          in order to set the piece of state (e.g. to a different value)
+          <br />
+          <br />
+          <button onClick={incrementValue}>Count = {count}</button>
+          <br />
+          <br />
+          The button is assigned the current value of count.
+          <br />
+          <br />
+          To increment this when the button is clicked, append the onClick event
+          attribute and pass it a function, for e.g.:
+          <br />
+          <br />
+          {`onClick={incrementValue}`}
+          <br />
+          <br />
+          Then before the return():
+          <br />
+          <br />
+          {`function incrementValue() {
           setCount(count + 1);
         }`}
-      </p>
-    </div>
+        </p>
+      </div>
+    </>
   );
 }
