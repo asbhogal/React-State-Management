b2a8bc26895f4bd5ecf1eabaea10b387a4e3d3e0
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-09-22 18:02:59 +0100

    improvements to accessibility

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function BackButton() {
   return (
     <Link to="/">
-      <button aria-label="Return to the home page">Back</button>
+      <button aria-label="Return back to the home page">Back</button>
     </Link>
   );
 }
